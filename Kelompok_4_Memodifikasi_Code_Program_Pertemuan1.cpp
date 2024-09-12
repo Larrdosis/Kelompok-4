388df08095a472304581b3825cd6db40b591356b
Larrdosis <ikanujung@gmail.com>
2024-09-12 12:38:04 +0700

    Add files via upload

---
+++ Kelompok_4_Memodifikasi_Code_Program_Pertemuan1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+	int i, n, jumlah;
+for (i=11; i>=1; i--){
+
+if (i%2 != 0) {
+cout << i << endl;
+jumlah = jumlah + i;
+}
+}
+cout << "Jumlah :  " << jumlah << endl;
+}
+
